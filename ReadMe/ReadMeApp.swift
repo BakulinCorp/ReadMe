9e78d689866fee1b4277f6f070f31a9f2b92ba23
BakulinCorp <87490884+BakulinCorp@users.noreply.github.com>
2022-07-13 18:10:47 +0300

    Create project v0.1

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Максим Бакулин on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
