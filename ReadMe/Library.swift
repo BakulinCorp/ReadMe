e27b1dab87b8009b730cb8151080ef0d38525f6f
BakulinCorp <87490884+BakulinCorp@users.noreply.github.com>
2022-07-31 11:24:13 +0300

    Environment v0.6
    and Fix bags

--- ReadMe/Library.swift
+++ ReadMe/Library.swift
@@ -33,11 +33,17 @@
 import SwiftUI
 
 
-struct Library {
+class Library: ObservableObject {
   var sortedBooks: [Book] { booksCache }
+    
+
+    func addNewBook(_ book: Book, image: Image?) {
+        booksCache.insert(book, at: 0)
+        images[book] = image
+    }
 
   /// An in-memory cache of the manually-sorted books.
-  private var booksCache: [Book] = [
+ @Published private var booksCache: [Book] = [
     .init(title: "Ein Neues Land", author: "Shaun Tan"),
     .init(title: "Bosch", author: "Laurinda Dixon", microReview: "Earthily Delightful."),
     .init(title: "Dare to Lead", author: "Brené Brown"),
@@ -51,6 +57,6 @@ struct Library {
     .init(title: "What to Say When You Talk to Yourself", author: "Shad Helmstetter")
   ]
     
-    var images: [Book: Image] = [:]
+    @Published var images: [Book: Image] = [:]
 }
         
--- ReadMe/Views/DetailView.swift
+++ ReadMe/Views/DetailView.swift
@@ -5,15 +5,12 @@
 //  Created by Максим Бакулин on 17.07.2022.
 //
 
-import class PhotosUI.PHPickerViewController
 import SwiftUI
 
 struct DetailView: View {
     @ObservedObject var book: Book
-    @Binding var image: Image?
-    @State var showingImagePicker = false
-    @State var showingDialog = false
-    
+    @EnvironmentObject var library: Library
+ 
     var body: some View {
         VStack(alignment: .leading) {
             HStack(spacing: 16) {
@@ -22,44 +19,14 @@ struct DetailView: View {
                 } label: {
                     Image(systemName: book.readMe ? "bookmark.fill" : "bookmark")
                         .font(.system(size: 48, weight: .light))
-            }
-            TitleAndAuthorStack(book: book, titleFont: .title,          authorFont: .title2)
-        }
-        VStack {
-            Divider()
-                .padding(.vertical)
-            TextField("Review...", text: $book.microReview)
-            Divider()
-                .padding(.vertical)
-            Book.Image(image: image, title: book.title, cornerRadius: 16)
-                .scaledToFit()
-            HStack {
-                if image != nil {
-                    Spacer()
-                    Button("Delete Image...") {
-                        showingDialog = true
-                    }
                 }
-                Spacer()
-                Button("Update Image...") {
-                    showingImagePicker = true
-                }
-                Spacer()
+                TitleAndAuthorStack(book: book, titleFont: .title, authorFont: .title2)
             }
-            .padding()
+            ReviewAndImageStack(book: book, image: $library.images[book])
         }
-        Spacer()
-    }
         .padding()
-        .sheet(isPresented: $showingImagePicker) {
-            PHPickerViewController.View(image: $image)
-        }
-        .confirmationDialog("Delete imgae for \(book.title)?", isPresented: $showingDialog) {
-            Button("Delete", role: .destructive) { image = nil}
-        } message: {
-            Text("Delete imgae for \(book.title)?")
-        }
-}
+     
+    }
 }
 
 
@@ -67,7 +34,9 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(book: .init(), image: .constant(nil))
+        DetailView(book: .init())
+            .environmentObject(Library())
             .previewedInAllColorSchemes
     }
 }
+
